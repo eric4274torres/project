d548c296652fa4ca41ef2a09b912da0cb1dcf460
project <eric42723@gmail.com>
2022-09-23 15:27:13 -0500

    Cambios

---
+++ src/components/HelloInput.vue
@@ -0,0 +1,16 @@
+<template>
+  <input type="button" :value=number>
+</template>
+
+<script>
+export default {
+    name:'HelloInput',
+    props:{
+        numero:Number
+    }
+}
+</script>
+
+<style>
+
+</style>
